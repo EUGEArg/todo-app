ad400fb767a65d2a101608f9fbd3ff61ad3c5350
EUGEarg <eugeniarivarossa@gmail.com>
2022-03-30 19:57:08 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Webpack Started
+
+Esqueleto de Webpack listo para trabajar sobre él.
+
+### Notas:
+
+Para reconstruir los módulos de Node:
+```
+npm install
+```
+
+Para construir el build:
+```
+npm run build
+```
+
+Para modo desarrollo (y vista previa):
+```
+npm start
+```
+
+
+
---
+++ webpack.prod.js
@@ -0,0 +1,77 @@
+const HtmlWebPack    = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin     = require('copy-webpack-plugin');
+
+const CssMinimizer   = require('css-minimizer-webpack-plugin');
+const TerserPlugin   = require('terser-webpack-plugin');
+
+module.exports = {
+    
+    mode: 'production',
+
+    output: {
+        clean: true,
+        filename: 'main.[contenthash].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
+        ]
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new TerserPlugin()
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].[fullhash].css', // De esta forma cambiamos el nombre del CSS cada vez que hacemos cambios
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+            { from: 'src/assets', to: 'assets' }
+            ]
+        })
+    ]
+}
